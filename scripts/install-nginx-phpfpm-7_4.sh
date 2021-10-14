16de6d13bc07d1a4d0796586b2848c22b4eeff0b
Martin Braun <martin.braun.1991@live.com>
2021-10-14 22:05:27 +0200

    Fix nginx phpfpm install script

--- scripts/install-nginx-phpfpm-7_4.sh
+++ scripts/install-nginx-phpfpm-7_4.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 apt install software-properties-common -y
-wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add - && echo "deb https://packages.sury.org/php/ buster main" | tee /etc/apt/sources.list.d/php.list && apt update
+apt install wget -y
+wget -q https://packages.sury.org/php/apt.gpg -O- | apt-key add - && echo "deb https://packages.sury.org/php/ buster main" | tee /etc/apt/sources.list.d/php.list && apt update
 apt install php7.4-fpm php7.4-common php7.4-mysql php7.4-gmp php7.4-curl php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-gd php7.4-xml php7.4-cli php7.4-zip php7.4-soap php7.4-imap -y
-echo "Please consider increasing memory_limit in /etc/php/7.4/fpm/php.ini"
+echo ""
+echo "Please consider increasing memory_limit in /etc/php/7.4/fpm/php.ini and run:"
+echo ""
+echo "    systemctl reload nginx"
 echo ""
