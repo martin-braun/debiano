d7d56e52644a8ead5bceaaf57ff2fc8cd109781d
Martin Braun <martin.braun.1991@live.com>
2022-07-13 12:13:20 +0200

    Fix branch name in debiano installation

--- scripts/install-debiano.sh
+++ scripts/install-debiano.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 trap 'echo "$ $BASH_COMMAND"' DEBUG
+set -e
 
 installroot=$HOME/git/gh/martin-braun
 if [ $1 ]; then
 	installroot=$1
 fi
 mkdir -p $installroot
+
 git clone -b desk git@github.com:martin-braun/debiano.git $installroot/debiano
 echo "source $installroot/debiano/utils/.bashrc" >> $HOME/.bashrc
 source $HOME/.bashrc
